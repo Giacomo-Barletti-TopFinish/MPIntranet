b433a406f9a38e6b4f60e4a6ed4c7a48083f78c8
VIAMATTEI\marco.chiti <chitimarco11@gmail.com>
2020-03-05 08:55:20 +0100

    modifiche schema

---
+++ Sorgenti/MPIntranet/MPIntranetWeb/Controllers/ManutenzioneController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MPIntranetWeb.Controllers
+{
+    public class ManutenzioneController : Controller
+    {
+        // GET: Manutenzione
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
