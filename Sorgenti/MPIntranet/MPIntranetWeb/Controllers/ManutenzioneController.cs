34d7c7080381723aeced93b1d1299f66db90c851
VIAMATTEI\marco.chiti <chitimarco11@gmail.com>
2020-03-05 14:34:30 +0100

    MODIFICHE HTML

--- Sorgenti/MPIntranet/MPIntranetWeb/Controllers/ManutenzioneController.cs
+++ Sorgenti/MPIntranet/MPIntranetWeb/Controllers/ManutenzioneController.cs
@@ -6,12 +6,39 @@ using System.Web.Mvc;
 
 namespace MPIntranetWeb.Controllers
 {
-    public class ManutenzioneController : Controller
+    public class ManutenzioneController : ControllerBase
     {
         // GET: Manutenzione
-        public ActionResult Index()
+        public ActionResult Ditte()
         {
+            //Manutenzione a = new Manutenzione();
+            //List<Ditte> lista = a.CreaListaDitte();
+
+            //return PartialView("CaricaDittePartial", lista);
             return View();
         }
+
+        public ActionResult RimuoviDitta(decimal idDitta)
+        {
+            //Manutenzione a = new Manutenzione();
+            //a.CancellaDitta(idDitta, ConnectedUser);
+            return null;
+        }
+
+        //public ActionResult CreaDitta(string ragioneSociale, decimal IdRiferimenti)
+        //{
+        //    Manutenzione a = new Manutenzione();
+        //    string messaggio = a.CreaDitta(ragioneSociale, IdRiferimenti, ConnectedUser);
+        //    return Content(messaggio);
+        //}
+
+        public ActionResult ModificaDitta(decimal idDitta, string ragioneSociale, decimal IdRiferimenti)
+        {
+            //Manutenzione a = new Manutenzione();
+            //a.ModificaBrand(idDitta, ragioneSociale, IdRiferimenti );
+            return null;
+        }
+
+
     }
 }
