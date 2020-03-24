410f04c3e86bf323cdf01468dc820b95a0f5a219
VIAMATTEI\giacomo.barletti <g.barletti@topfinish.it>
2020-03-24 07:42:08 +0100

    modifica pereliminazione termine turbula

---
+++ Sorgenti/MPIntranet/MPIntranetWeb/Controllers/DocumentiController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MPIntranetWeb.Controllers
+{
+    public class DocumentiController : ControllerBase
+    {
+
+        public ActionResult MODIFICADOCUMENTO()
+        {
+            return View();
+        }
+        public ActionResult AGGIUNGIDOCUMENTO()
+        {
+            return View();
+        }
+        public ActionResult RIMUOVIDOCUMENTO()
+        {
+            return View();
+        }
+    }
+}
