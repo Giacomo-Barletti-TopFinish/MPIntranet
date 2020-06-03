a14ce0123c9efedcae095827ce7fe1daf6b66841
VIAMATTEI\giacomo.barletti <g.barletti@topfinish.it>
2020-06-03 09:58:33 +0200

    riallineamento

---
+++ Sorgenti/MPIntranet/MPIntranetWeb/Controllers/ReportController.cs
@@ -0,0 +1,39 @@
+﻿using MPIntranet.Business;
+using MPIntranet.Entities;
+using MPIntranet.Models.Anagrafica;
+using MPIntranet.Models.Common;
+using MPIntranet.Models.Report;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MPIntranetWeb.Controllers
+{
+    public class ReportController : ControllerBase
+    {
+        public ActionResult CaricoLavoro()
+        {
+            Anagrafica anagrafica = new Anagrafica();
+            List<RepartoModel> reparti = anagrafica.CreaListaRepartoModel();
+
+            List<MPIntranetListItem> repartiList = reparti.Select(x => new MPIntranetListItem(x.Descrizione, x.IdReparto.ToString())).ToList();
+            repartiList.Insert(0, new MPIntranetListItem("** TUTTI I REPARTI **", ElementiVuoti.TuttiReparti.ToString()));
+
+            ViewData.Add("Reparti", repartiList);
+
+
+            return View();
+        }
+
+        public ActionResult CaricaCaricoReparto(decimal idReparto)
+        {
+            Report report = new Report();
+            List<CaricoRepartoModel> caricoLavoroList = report.CreaListaCaricoReparto(idReparto);
+
+
+            return PartialView("CaricoLavoroPartial", caricoLavoroList);
+        }
+    }
+}
